d15354ce2a2f5be023d3d8f9ebfc86a95c12788c
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-09-08 12:34:00 +0200

    Fix duplicate code

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
 terraform {
+  required_version = ">= 0.13.0"
   required_providers {
     hsdp = {
       source  = "philips-software/hsdp"
       version = ">= 0.6.1"
+    }
+    random = {
+      source  = "random"
+      version = ">= 2.2.1"
+    }
+    null = {
+      source  = "null"
+      version = ">= 2.2.1"
     }
   }
 }
