2fdfabe1205cf734dbb9f13e4cea750fa9b085ef
andriy.pentsak <andriy.pentsak@vitech.com.ua>
2020-11-17 12:35:47 +0200

    change ports

--- scripts/container_exporter.sh
+++ scripts/container_exporter.sh
@@ -1,9 +1,9 @@
 docker rm -fv cadvisor_exporter
 docker rm -fv node_exporter
 
-docker run -d --name=cadvisor_exporter --device=/dev/kmsg -p 8080:8080 \
+docker run -d --name=cadvisor_exporter --device=/dev/kmsg -p 9102:8080 \
 -v /:/rootfs:ro -v /var/run:/var/run:ro -v /sys:/sys:ro \
 -v /var/lib/docker/:/var/lib/docker:ro -v /dev/disk/:/dev/disk:ro \
 gcr.io/cadvisor/cadvisor:v0.38.1
 
-docker run -d --name node_exporter -p 9110:9100 bitnami/node-exporter:latest
+docker run -d --name node_exporter -p 9101:9100 bitnami/node-exporter:latest
